c6f9721f206d6fafdf36a421a2d8e7458cd54af5
Javier Misat <rjmisal@misena.edu.co>
2018-03-30 14:29:27 -0500

    Agregado normalize y estilos a theme

--- wp-content/themes/mana-express/functions.php
+++ wp-content/themes/mana-express/functions.php
@@ -1,2 +1,18 @@
 <?php
 
+ function manaexpress_styles()
+ {
+ 	/*
+ 	 * Registrar estilos css
+ 	 */
+	wp_register_style('normalize', get_template_directory_uri().'/css/normalize.css', array(), '8.0.0');
+	wp_register_style('style', get_template_directory_uri().'/style.css', 'normalize', '1.0');
+
+	/*
+	 * Invocar estilos css agregandolos al head
+	 */
+	 wp_enqueue_style('normalize');
+	 wp_enqueue_style('style');
+ }
+
+ add_action('wp_enqueue_scripts', 'manaexpress_styles');
--- wp-content/themes/mana-express/index.php
+++ wp-content/themes/mana-express/index.php
@@ -1,12 +1,20 @@
-<?php
+<!doctype html>
+<html lang="es_ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= the_title() ?></title>
 
-while ( have_posts() ):
-	the_post();
+	<?php wp_head(); ?>
 
-	the_date();
 
-	the_title( '<h1>', '</h1>' );
+</head>
+<body>
 
-	the_content();
 
-endwhile;
+
+
+</body>
+</html>
