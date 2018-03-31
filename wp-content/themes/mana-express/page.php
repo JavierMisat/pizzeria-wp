d2c67a35f1321c0894edf9b6c0a1ddf46fc9020b
Javier Misat <rjmisal@misena.edu.co>
2018-03-31 00:07:41 -0500

    Imagen destacada aÃadida y estilos terminados

--- wp-content/themes/mana-express/page.php
+++ wp-content/themes/mana-express/page.php
@@ -2,12 +2,16 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-    <?php the_post_thumbnail('medium'); ?>
-
-    <h1><?php the_title(); ?></h1>
+    <div class="hero"  style="background-image: url(<?= get_the_post_thumbnail_url(); ?>)">
+        <div class="contenido-hero">
+            <div class="texto-hero">
+               <?php the_title('<h1>', '</h1>'); ?>
+            </div>
+        </div>
+    </div>
 
     <div class="principal contenedor">
-        <main>
+        <main class="text-centrado contenido-paginas">
             <?php the_content(); ?>
         </main>
     </div>
