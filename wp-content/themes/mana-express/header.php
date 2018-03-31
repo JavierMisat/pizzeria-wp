6c0ed40f4c4c78f87d847c5e5b5d1248a7213ebd
Javier Misat <rjmisal@misena.edu.co>
2018-03-30 19:09:05 -0500

    Estilos al menu principal Please enter the commit message for your changes. Lines starting

--- wp-content/themes/mana-express/header.php
+++ wp-content/themes/mana-express/header.php
@@ -20,44 +20,41 @@
 
         <div class="informacion-encabezado">
             <div class="redes-sociales">
-	            <?php
-	            $args =
-		            [
-			            'theme_location'  => 'social-menu',
-			            'container'       => 'nav',
-			            'container_class' => 'sociales',
-			            'container_id'    => 'sociales',
-                        'link_before' => '<span class="screen-reader-text">',
-                        'link_after' => '</span>',
-                        '',
-		            ];
+				<?php
+				$args =
+					[
+						'theme_location'  => 'social-menu',
+						'container'       => 'nav',
+						'container_class' => 'sociales',
+						'container_id'    => 'sociales',
+						'link_before'     => '<span class="screen-reader-text">',
+						'link_after'      => '</span>',
+						'',
+					];
 
-	            wp_nav_menu( $args );
-	            ?>
+				wp_nav_menu( $args );
+				?>
             </div><!--.redes-sociales -->
 
             <div class="direccion">
                 <p>Calle 152, Cra. 116 #152 - 69, Bogotá </p>
                 <p>Teléfono: (1) 6971829</p>
             </div>
-            
+
         </div> <!--.informacion-encabezado-->
 
     </div>  <!--.contenedor -->
 </header>
 
-<nav class="menu-sitio">
-    <div class="contenedor navegacion">
-		<?php
-		$args =
-			[
-				'theme_location'  => 'header-menu',
-				'container'       => 'nav',
-				'container_class' => 'menu-header',
-				'container_id'    => 'menu-header',
-			];
+<div class="contenedor navegacion">
+	<?php
+	$args =
+		[
+			'theme_location'  => 'header-menu',
+			'container'       => 'nav',
+			'container_class' => 'menu-sitio',
+		];
 
-		wp_nav_menu( $args );
-		?>
-    </div>
-</nav>
+	wp_nav_menu( $args );
+	?>
+</div>
