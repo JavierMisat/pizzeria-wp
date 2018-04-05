e815705e65b980b2895500520a05e06a375d603a
Javier Misat <rjmisal@misena.edu.co>
2018-04-05 10:52:02 -0500

    Terminado estilos de comentarios de blog

--- wp-content/themes/mana-express/single.php
+++ wp-content/themes/mana-express/single.php
@@ -2,23 +2,44 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<div class="hero"  style="background-image: url(<?= get_the_post_thumbnail_url(); ?>)">
-		<div class="contenido-hero">
-			<div class="texto-hero">
-				<?php the_title('<h1>', '</h1>'); ?>
-			</div>
-		</div>
-	</div>
+    <div class="hero" style="background-image: url(<?= get_the_post_thumbnail_url(); ?>)">
+        <div class="contenido-hero">
+            <div class="texto-hero">
+				<?php the_title( '<h1>', '</h1>' ); ?>
+            </div>
+        </div>
+    </div>
 
-	<div class="principal contenedor">
-		<main class="text-centrado contenido-paginas">
+    <div class="principal contenedor">
+        <main class="text-centrado contenido-paginas">
 			<?php the_content(); ?>
-		</main>
-	</div>
+        </main>
+    </div>
 
-	<div class="contenedor comentarios">
+    <div class="contenedor comentarios">
 		<?php comment_form(); ?>
-	</div>
+    </div>
+
+    <div class="contenedor">
+        <ul class="lista-comentarios">
+			<?php $comentarios = get_comments(
+				[
+					'post_id' => $post->ID,
+					'status'  => 'approve'
+				]
+			);
+
+			wp_list_comments(
+				[
+					'per_apeg'          => 10,
+					'reverse_top_level' => false,
+
+				], $comentarios
+			);
+
+			?>
+        </ul>
+    </div>
 
 <?php endwhile; ?>
 
