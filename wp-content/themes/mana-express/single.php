0f17947365517de5aeb998b2e78800eeb8c5c4f3
Javier Misat <rjmisal@misena.edu.co>
2018-04-04 23:11:22 -0500

    Terminado el formulario de comentarios

---
+++ wp-content/themes/mana-express/single.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<div class="hero"  style="background-image: url(<?= get_the_post_thumbnail_url(); ?>)">
+		<div class="contenido-hero">
+			<div class="texto-hero">
+				<?php the_title('<h1>', '</h1>'); ?>
+			</div>
+		</div>
+	</div>
+
+	<div class="principal contenedor">
+		<main class="text-centrado contenido-paginas">
+			<?php the_content(); ?>
+		</main>
+	</div>
+
+	<div class="contenedor comentarios">
+		<?php comment_form(); ?>
+	</div>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
