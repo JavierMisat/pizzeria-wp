1b9bd3c094bdc03adc289397cae1b9a574488e00
Javier Misat <rjmisal@misena.edu.co>
2018-04-07 00:23:10 -0500

    Agregado fluidbox plugin

--- wp-content/themes/mana-express/functions.php
+++ wp-content/themes/mana-express/functions.php
@@ -20,6 +20,7 @@ function manaexpress_styles() {
 	wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '5.0.9' );
 	wp_register_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900', array(), '1.0.0' );
 	wp_register_style( 'fontawesome', get_template_directory_uri() . '/css/fontawesome-all.min.css', 'normalize', '8.0.0' );
+	wp_register_style( 'fluidboxcss', get_template_directory_uri() . '/css/fluidbox.min.css', 'normalize', '2.0.5' );
 	wp_register_style( 'style', get_template_directory_uri() . '/style.css', 'normalize', '1.0' );
 
 
@@ -28,14 +29,17 @@ function manaexpress_styles() {
 	 */
 	wp_enqueue_style( 'normalize' );
 	wp_enqueue_style( 'fontawesome' );
+	wp_enqueue_style( 'fluidboxcss' );
 	wp_enqueue_style( 'style' );
 
 	/**
 	 * Regusistar Javascript
 	 */
+	wp_register_script( 'fluidbox', get_template_directory_uri() . '/js/jquery.fluidbox.min.js', array(), '1.0.0', true );
 	wp_register_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array(), '1.0.0', true );
 
 	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'fluidbox' );
 	wp_enqueue_script( 'scripts' );
 
 }
