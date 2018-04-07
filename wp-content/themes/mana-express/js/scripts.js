cab38b78983f562234f9840a82466b1658ec0c42
Javier Misat <rjmisal@misena.edu.co>
2018-04-07 00:40:24 -0500

    Implementado fluidbox

--- wp-content/themes/mana-express/js/scripts.js
+++ wp-content/themes/mana-express/js/scripts.js
@@ -13,10 +13,22 @@ $(document).ready(function () {
 
     $(window).resize(function (e) {
         var windowSize = $(document).width();
-        if (windowSize >= breakpoint){
-           $('nav.menu-sitio').show();
-       }else {
-           $('nav.menu-sitio').hide();
-       }
+        if (windowSize >= breakpoint) {
+            $('nav.menu-sitio').show();
+        } else {
+            $('nav.menu-sitio').hide();
+        }
     });
+
+    /**
+     * Fluidbox
+     */
+    jQuery('.gallery a').each(function () {
+        jQuery(this).attr({'data-fluidbox': ''});
+    });
+
+    if($('[data-fluidbox]').length > 0){
+        jQuery('[data-fluidbox]').fluidbox();
+    }
+
 });
